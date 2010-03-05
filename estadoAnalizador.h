a362e446a25aa865db1388d4790320e6af14823e
theom3ga <theom3ga@7c664bb7-78ef-b7b4-8bb6-3cce6fcafa8d>
2010-03-05 11:24:05 +0000

    Desarrollando el módulo de análisis

---
+++ estadoAnalizador.h
@@ -0,0 +1,27 @@
+#ifndef _ESTADOANALIZADOR_H_
+#define _ESTADOANALIZADOR_H_
+
+#include "estado.h"
+#include "analizador.h"
+
+
+#include "boost/scoped_ptr.hpp"
+#include <string>
+
+class Juego;
+
+class estadoAnalizador : public estado{
+    bool lanzado;
+    boost::scoped_ptr<Gosu::Image> imgFondo;
+    boost::scoped_ptr<Analizador> analizador;
+    
+public:
+    estadoAnalizador(Juego * p);
+    void lanzar ();
+    void update();
+    void draw();
+    void buttonDown(Gosu::Button boton);
+    
+};
+
+#endif
