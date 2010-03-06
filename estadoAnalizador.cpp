a13e38eb188c5cba0c8d0cb408075239f4df49d9
theom3ga <theom3ga@7c664bb7-78ef-b7b4-8bb6-3cce6fcafa8d>
2010-03-06 01:56:50 +0000

    No comment

--- estadoAnalizador.cpp
+++ estadoAnalizador.cpp
@@ -27,6 +27,27 @@ estadoAnalizador::estadoAnalizador (Juego * p) : estado(p), numSamples(2048){
     lanzar();
     miBuffer.pos = 0;
 
+    notas[523.25] = Do5;
+    notas[592.163] = Re5;
+    notas[656.763] = Mi5;
+    notas[699.829] = Fa5;
+    notas[785.692] = Sol5;
+    notas[893.628] = La5;
+    notas[1001.29] = Si5;
+    notas[1076.66] = Do6;
+    notas[1195.09] = Re6;
+
+    imgFondo.reset(new Gosu::Image(padre -> graphics(), Gosu::resourcePrefix() + L"media/analizadorAssets/baseAnalizador.png"));
+    imgDo5.reset(new Gosu::Image(padre -> graphics(), Gosu::resourcePrefix() + L"media/analizadorAssets/do5.png"));
+    imgRe5.reset(new Gosu::Image(padre -> graphics(), Gosu::resourcePrefix() + L"media/analizadorAssets/re5.png"));
+    imgMi5.reset(new Gosu::Image(padre -> graphics(), Gosu::resourcePrefix() + L"media/analizadorAssets/mi5.png"));
+    imgFa5.reset(new Gosu::Image(padre -> graphics(), Gosu::resourcePrefix() + L"media/analizadorAssets/fa5.png"));
+    imgSol5.reset(new Gosu::Image(padre -> graphics(), Gosu::resourcePrefix() + L"media/analizadorAssets/sol5.png"));
+    imgLa5.reset(new Gosu::Image(padre -> graphics(), Gosu::resourcePrefix() + L"media/analizadorAssets/la5.png"));
+    imgSi5.reset(new Gosu::Image(padre -> graphics(), Gosu::resourcePrefix() + L"media/analizadorAssets/si5.png"));
+    imgDo6.reset(new Gosu::Image(padre -> graphics(), Gosu::resourcePrefix() + L"media/analizadorAssets/do6.png"));
+    imgRe6.reset(new Gosu::Image(padre -> graphics(), Gosu::resourcePrefix() + L"media/analizadorAssets/re6.png"));
+
 }
 
 void estadoAnalizador::lanzar(){
@@ -50,6 +71,30 @@ void estadoAnalizador::update(){
 void estadoAnalizador::draw(){
     if(!lanzado) 
 	return;
+
+    boost::shared_ptr<Gosu::Image> p;
+    switch(notaActual()){
+    case Do5:
+	p = imgDo5; break;
+    case Re5:
+	p = imgRe5; break;
+    case Mi5:
+	p = imgMi5; break;
+    case Fa5:
+	p = imgFa5; break;
+    case Sol5:
+	p = imgSol5; break;
+    case La5:
+	p = imgLa5; break;
+    case Si5:
+	p = imgSi5; break;
+    case Do6:
+	p = imgDo6; break;
+    case Re6:
+	p = imgRe6; break;
+    }
+    p -> draw(584,138,2);
+    imgFondo -> draw(0,0,1);
 }
 
 void estadoAnalizador::buttonDown(Gosu::Button boton){
@@ -67,22 +112,29 @@ estadoAnalizador::~estadoAnalizador(){
 bool estadoAnalizador::configurarFlujo(){
 //    PaStreamParameters inParameters, outParameters;
 
+    cout << "- Intentando Pa_Initialize..." << flush;
     err = Pa_Initialize();
-
     if(err != paNoError) std::cout << "ERROR: " << (int) err << std::endl;
 
+    cout << "OK" << endl;
+
+    cout << "- Intentando Pa_OpenDefaultStream..." << flush;
     err = Pa_OpenDefaultStream(&stream,
 			       2,
 			       0,
 			       paInt16,
 			       44100,
 			       256,
-			       updateBuffer,
+			       &updateBuffer,
 			       (void*) this);
+
+    if(err != paNoError) std::cout << "ERROR" << std::endl;
+
+    cout << "OK" << endl;
 
     std::cout << "* Duración del búffer: " << 256.0/44100.0*1000 << "ms" << std::endl;
 
-    if(err != paNoError) std::cout << "ERROR" << std::endl;
+
 
     return true;
 } 
@@ -145,11 +197,6 @@ int estadoAnalizador::updateBuffer(const void * inB,
 	miBuffer.pos++;
     }
 
-/*
-    if( miBuffer.pos > 4095){
-	 miBuffer.pos = 0;
-    }//*/
-
     if( miBuffer.pos > 4095){
 	 miBuffer.pos = 0; 
 	WindowFunc(3, 4096,  miBuffer.in);
@@ -177,22 +224,25 @@ int estadoAnalizador::updateBuffer(const void * inB,
 		  << std::setw(12) << maxValue[0]
 		  << std::flush;	//
 
-/*
-	 puntero -> mayores[0] = maxPos[0] * 22050 / 2048;
-	 puntero -> mayores[1] = maxPos[1] * 22050 / 2048;
-	 puntero -> mayores[2] = maxPos[2] * 22050 / 2048;
+
+	 miBuffer . mayores[0] = maxPos[0] * 22050 / 2048;
+	 miBuffer . mayores[1] = maxPos[1] * 22050 / 2048;
+	 miBuffer . mayores[2] = maxPos[2] * 22050 / 2048;
 	//outputLog << maxPos << std::endl;
 
-	 puntero -> silencio = ((maxValue[0] < 1e+16)?true:false);//*/
+//	 puntero -> silencio = ((maxValue[0] < 1e+16)?true:false);//*/
     }//*/
 
     return paContinue;
 
 }
 
+t_altura estadoAnalizador::notaActual(){
+    return asociarNota(miBuffer .mayores[0]);
+}
 
-//t_altura estadoAnalizador::asociarNota(double frecuencia){
-/*    std::map<double, t_altura> diferencias;
+t_altura estadoAnalizador::asociarNota(double frecuencia){
+    std::map<double, t_altura> diferencias;
     std::map<double, t_altura>::iterator iter;
 
     for(iter = notas.begin();
@@ -205,4 +255,4 @@ int estadoAnalizador::updateBuffer(const void * inB,
     iter = min_element(diferencias.begin(), diferencias.end());
     return iter -> second;//*/
     
-//}
+}
