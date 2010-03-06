6b779a65a097b3d7055635600f432c7d8f73c8aa
theom3ga <theom3ga@7c664bb7-78ef-b7b4-8bb6-3cce6fcafa8d>
2010-03-06 01:00:34 +0000

    Al final haciendo el búffer estático se arreglan las cosas. Supongo que acceder mediante puntero en nivel de interrupción hacía que petara el sistema

--- estadoAnalizador.h
+++ estadoAnalizador.h
@@ -2,25 +2,75 @@
 #define _ESTADOANALIZADOR_H_
 
 #include "estado.h"
-#include "analizador.h"
+#include "juego.h"
 
-
+#include <Gosu/Gosu.hpp>
 #include "boost/scoped_ptr.hpp"
+
+#include "portaudio.h"
+
+
+#include "global.h"
+
 #include <string>
+#include <map>
 
 class Juego;
+
+class tipoBuffer{
+public:
+    tipoBuffer():pos(0){}
+    int pos;
+    float in[4200]; // 4096
+    float out[2048];
+};
 
 class estadoAnalizador : public estado{
     bool lanzado;
     boost::scoped_ptr<Gosu::Image> imgFondo;
-    Analizador * analizador;
+
+    bool iniciado, silencio;
+
+    // Vector de harmónicos más importantes en la nota
+    float mayores[5];
+
+    // Flujo principal
+    PaStream * stream;
+
+    PaError err;
+
+    // Mapa de frecuencias y notas
+//    std::map<double, t_altura> notas;
+
+    // Devuelve la nota asociada a una frecuencia
+//    t_altura asociarNota(double frecuencia);
+
+    // Función callback    
+    static int updateBuffer(const void * inB, 
+			    void * outB, 
+			    unsigned long nFrames, 
+			    const PaStreamCallbackTimeInfo * timeInfo,
+			    PaStreamCallbackFlags statusFlags,
+			    void * data);
+    int updateBuffer2(const void * inB, 
+			    void * outB, 
+			    unsigned long nFrames, 
+			    const PaStreamCallbackTimeInfo * timeInfo,
+		      PaStreamCallbackFlags statusFlags);
     
 public:
+    int numSamples;
+
+    static tipoBuffer miBuffer;
     estadoAnalizador(Juego * p);
     void lanzar ();
     void update();
     void draw();
     void buttonDown(Gosu::Button boton);
+    bool configurarFlujo();
+    bool iniciarAnalisis();
+//    t_altura notaActual();
+    bool detenerAnalisis();
     ~estadoAnalizador();
 };
 
