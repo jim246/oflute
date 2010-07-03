0ece8b78ba00d967b5e2348437eab2ac223c745d
theom3ga <theom3ga@7c664bb7-78ef-b7b4-8bb6-3cce6fcafa8d>
2010-07-03 12:21:13 +0000

    Añadidas figuras de los silencios, de las notas invertidas y figura blanca.

--- trunk/nota.h
+++ trunk/nota.h
@@ -21,16 +21,47 @@ struct Nota{
 
     Nota(Gosu::Graphics & g, t_altura altura, t_figura figura, float tiempos) : 
 	altura(altura), figura(figura), tiemposDelante(tiempos){
+	wstring rutaImg = L" ";
 
 	if(figura == Redonda){
-	    imagen.reset ( new Gosu::Image (g, L"media/figuras/figRedonda.png"));
-	}else if(figura == Blanca){
-	    imagen.reset ( new Gosu::Image (g, L"media/figuras/figBlanca.png"));
-	}else if(figura == Corchea){
-	    imagen.reset ( new Gosu::Image (g, L"media/figuras/figCorchea.png"));
-	}else{
-	    imagen.reset ( new Gosu::Image (g, L"media/figuras/figNegra.png"));
+	    if(altura == Silencio){
+		rutaImg = L"media/figuras/silRedonda.png";
+	    }else{
+		rutaImg = L"media/figuras/figRedonda.png";
+	    }
 	}
+
+	else if(figura == Blanca){
+	    if(altura == Silencio){
+		rutaImg = L"media/figuras/silBlanca.png";
+	    }else if(altura <= Si5){
+		rutaImg = L"media/figuras/figBlanca.png";
+	    }else{
+		rutaImg = L"media/figuras/figBlancaInv.png";
+	    }
+	}
+
+	else if(figura == Corchea){
+	    if(altura == Silencio){
+		rutaImg = L"media/figuras/silCorchea.png";
+	    }else if(altura <= Si5){
+		rutaImg = L"media/figuras/figCorchea.png";
+	    }else{
+		rutaImg = L"media/figuras/figCorcheaInv.png";
+	    }
+	}
+
+	else{
+	    if(altura == Silencio){
+		rutaImg = L"media/figuras/silNegra.png";
+	    }else if(altura <= Si5){
+		rutaImg = L"media/figuras/figNegra.png";
+	    }else{
+		rutaImg = L"media/figuras/figNegraInv.png";
+	    }
+	}
+
+	imagen.reset ( new Gosu::Image (g, rutaImg));
 
 	lDEBUG << Log::CON("Nota") 
 	       << boost::format(" %i %i %f") % altura % figura % tiempos;
@@ -51,8 +82,24 @@ struct Nota{
 	}
     }
 
-    static float devolverAltura(t_altura t){
-	return (9 - t) * 18.6;
+    static float devolverAltura(t_altura t, t_figura fig = Nulo){
+
+	if(t != Silencio){
+	    return (9 - t) * 18.6;
+	}else{
+	    if(fig == Redonda ){
+		return 18;
+	    }
+
+	    else if(fig == Blanca){
+		return 19;
+	    }
+
+	    else if(fig == Negra || fig == Corchea){
+		return 18.6 * 5;
+	    }
+	    return 0;
+	}
     }
 
     void updatePos(float x_){
@@ -61,7 +108,7 @@ struct Nota{
 
     void draw(){
 	if(x > -100)
-	    imagen -> draw(x, 162 + Nota::devolverAltura(altura), 5);
+	    imagen -> draw(x, 162 + Nota::devolverAltura(altura, figura), 5);
     }
 };
 
